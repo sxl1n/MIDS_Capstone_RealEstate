9c47d734d2b15db0605423fceb843acb74b8b5a0
Judd Heckman <jheckman@ischool.berkeley.edu>
2015-08-08 16:00:47 -0700

    fixed zip_distance parameters, merged_fat.csv includes all CA zips

--- ZipDistance.py
+++ ZipDistance.py
@@ -1,22 +1,25 @@
 import pandas as pd
 from sklearn.metrics.pairwise import euclidean_distances
-import numpy as np
 
-def zip_distance(given_zip, given_pref, target_zips):
+def zip_distance(given_zip, target_zips):
     fileName = "merged_fat.csv"
     df = pd.read_csv(fileName)
-    df = df.dropna(subset=df.columns[-5:])
+    df_targets = df[df.zip.isin(target_zips)]
 
-    given_zip_data = df[df['zip'] == given_zip]
+    given_zip_data = df[df.zip == given_zip]
 
-    zip_dist = euclidean_distances(df.values, given_zip_data.values)
-    dist_list = zip_dist.reshape(df.shape[0],)
-    print dist_list
-    zip_list = df.ix[:,0].astype(int)
+    zip_dist = euclidean_distances(df_targets.values, given_zip_data.values)
+    dist_list = zip_dist.reshape(df_targets.shape[0],)
+
+    zip_list = df_targets.ix[:,0].astype(int)
     zip_dist_dict = dict(zip(zip_list, dist_list.T))
     return zip_dist_dict
 
 if __name__ == "__main__":
-    zip_scores = zip_distance(94133, 1, 1)
-    print zip_scores
+    bay_zip_data = pd.read_csv("zip_code_database_bay_area.csv")
+    bay_zips = bay_zip_data.zip.values
+    zip_scores = zip_distance(92663, bay_zips)
+
+    zip_scores_ordered = sorted(zip_scores.items(), key=lambda z: z[1])
+    print zip_scores_ordered
 
